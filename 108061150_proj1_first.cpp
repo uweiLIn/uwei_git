9cecd3d0f06ce9afc5c01de7479cc1e6017ad4ee
uweiLin <Lin33113311@gmail.com>
2020-10-12 22:48:21 +0800

    commit on ' master'

--- 108061150_proj1_first.cpp
+++ 108061150_proj1_first.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main(int argc, char *argv[]) {
+class addmatrix{
+public:
 
-
+private:
+    int row;
+    int column;
+    int value;
+    
+};
+int main(int argc, char* argv[]) {
+    
+    ifstream infile(argv[1]);     
+    if (!infile) {
+    cout << "Can not open file!\n";
+    return 1;
+    }
+    else
+   
+   return 0;
 }
